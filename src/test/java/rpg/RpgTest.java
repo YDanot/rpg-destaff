cdc95fb29b8540efd2fb5dc2dde06b545640fc27
YDanot <y.danot@gmail.com>
2018-12-10 15:09:48 +0100

    new_character_should_start_with_1000_HP

--- src/test/java/rpg/RpgTest.java
+++ src/test/java/rpg/RpgTest.java
@@ -1,11 +1,19 @@
 package rpg;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class RpgTest {
 
     @Test
-    public void aTest() {
+    public void new_character_should_start_with_1000_HP() {
+        final RpgCharacter rpgCharacter = new RpgCharacter();
+        Assertions.assertThat(rpgCharacter.health()).isEqualTo(1000);
+    }
 
+    private class RpgCharacter {
+        public int health() {
+            return 1000;
+        }
     }
 }
---
+++ tcrdd.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+BRANCH=origin/master
+
+function runTest() {
+    mvn clean test
+}
+
+function testJustAdded(){
+    [[ ! -z `git diff HEAD | grep "^\+.*@Test"` ]]
+}
+
+function notSynchronisedYet(){
+    [[ ! -z `git diff ${BRANCH} HEAD` ]]
+}
+
+function commit() {
+    git add . && \
+    if testJustAdded; then
+        git commit
+    elif notSynchronisedYet; then
+        git commit --amend --no-edit
+    else
+        git commit --allow-empty-message -m ""
+    fi
+}
+
+function revert() {
+    git reset --hard
+    git clean -f
+}
+
+function sync() {
+    git rebase ${BRANCH} \
+    && runTest \
+    && git push
+}
+
+KNOWN_AS_GREEN=false
+KNOWN_AS_RED=false
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case ${key} in
+        -g|--green)
+            KNOWN_AS_GREEN=true
+            shift
+            ;;
+        -r|--red)
+            KNOWN_AS_RED=true
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if ${KNOWN_AS_RED} || (! ${KNOWN_AS_GREEN} && testJustAdded)
+then
+    runTest && revert || commit
+else
+    runTest && (commit && sync) || revert
+fi
